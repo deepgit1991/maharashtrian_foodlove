b330f3530a2426c4f6f53cd4b51bf6c6cff9b7cd
deepika <deepika.kamble@techno.soft.in>
2026-05-26 15:02:36 +0530

    changes in api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000"
-    : "https://maharashtrian-foodstore.onrender.com";
+    : "https://maharashtrian-foodlove.onrender.com";
 
 export default API_URL;
